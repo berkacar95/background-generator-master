fbcb0ce896e97f6c9e763a35860b99a5e7af9d48
berkacar95 <60269370+berkacar95@users.noreply.github.com>
2021-01-29 19:29:06 +0300

    major update

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
-var _ = require('lodash');
-console.log(_);
+//Newer modern way to write the code (this new syntax will be explored in Advanced JS section)
+const css = document.querySelector('h3');
+const [color1, color2] = document.querySelectorAll('.color')
+const body = document.querySelector('#gradient');
 
-var array = [1, 2, 3, 4, 5];
-console.log('answer:', _.without(array, 3));
-
-var css = document.querySelector("h3");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body = document.getElementById("gradient")
-
-function setGradient() {
-    body.style.background = "linear-gradient(to right, "
-        + color1.value + ", " + color2.value + ")";
-
-    css.textContent = body.style.background + ";"
+setGradient = () => {
+	body.style.background = _linearGradient();
+	css.textContent = `${body.style.background};`;
 }
 
-// color1.addEventListener("input", setGradient);
-// color2.addEventListener("input", setGradient);
+_linearGradient = () => {
+	return `linear-gradient(to right, ${color1.value}, ${color2.value})`;
+}
 
+color1.addEventListener('input', setGradient);
+
+color2.addEventListener('input', setGradient);
+
+//Original version:
+// var css = document.querySelector("h3");
+// var color1 = document.querySelector(".color1");
+// var color2 = document.querySelector(".color2");
+// var body = document.getElementById("gradient");
+
+// function setGradient() {
+// 	body.style.background = 
+// 	"linear-gradient(to right, " 
+// 	+ color1.value 
+// 	+ ", " 
+// 	+ color2.value 
+// 	+ ")";
+
+// 	css.textContent = body.style.background + ";";
+// }
+
+// color1.addEventListener("input", setGradient);
+
+// color2.addEventListener("input", setGradient); 
